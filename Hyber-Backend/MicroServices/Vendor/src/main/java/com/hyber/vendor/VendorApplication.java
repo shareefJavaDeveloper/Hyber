1e66763bd377e304b344c2898a4b21c83acbe98a
shareef <shareef@example.com>
2021-12-16 16:16:45 +0530

    Intial Commit

---
+++ Hyber-Backend/MicroServices/Vendor/src/main/java/com/hyber/vendor/VendorApplication.java
@@ -0,0 +1,15 @@
+package com.hyber.vendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class VendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorApplication.class, args);
+	}
+
+}
---
+++ Hyber-Backend/MicroServices/Vendor/src/test/java/com/hyber/vendor/VendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyber.vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
